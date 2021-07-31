e32a640e61fe42aa415a22eb8bc75ca83d247837
dmitry_marach <dmitrymarach2000@yandex.ru>
2021-07-31 14:39:48 +0300

    Refactoring. Fixing caps in ImgNationBackApplication

--- src/main/java/ru/svaosoft/imgnationback/ImgNationBackApplication.java
+++ src/main/java/ru/svaosoft/imgnationback/ImgNationBackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImGnationBackApplication {
+public class ImgNationBackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ImGnationBackApplication.class, args);
+        SpringApplication.run(ImgNationBackApplication.class, args);
     }
 
 }
