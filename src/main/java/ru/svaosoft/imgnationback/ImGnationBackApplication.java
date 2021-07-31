af2456f6f67a8f63d54bf97c0baefc793241ae6d
dmitry_marach <dmitrymarach2000@yandex.ru>
2021-07-31 14:36:44 +0300

    Create a new Spring Boot project
    close#1

---
+++ src/main/java/ru/svaosoft/imgnationback/ImGnationBackApplication.java
@@ -0,0 +1,13 @@
+package ru.svaosoft.imgnationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImGnationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImGnationBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/svaosoft/imgnationback/ImGnationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svaosoft.imgnationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImGnationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
